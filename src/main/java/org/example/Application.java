179255065cd4a8e75c77004413af6e6256087dcb
Matti Tahvonen <matti@vaadin.com>
2025-11-10 20:31:03 +0200

    work on V25/SB4

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,10 +1,15 @@
 package org.example;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+@StyleSheet(Lumo.STYLESHEET)
+@StyleSheet(Lumo.UTILITY_STYLESHEET)
+public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
